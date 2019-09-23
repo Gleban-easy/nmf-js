3f54bfa348106151708274d28957cbb442ea6411
pl0q1n <pl0q1n@gmail.com>
2019-09-23 23:46:50 +0400

    adding get request as postlist component

---
+++ src/components/PostList.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react'
+import axios from 'axios'
+
+class PostList extends Component {
+	constructor(props) {
+		super(props)
+		this.config = {
+	    	headers: {
+	    		'Accept': 'application/json',
+    			'Content-Type': 'application/json',
+	    	}
+		};
+		this.state = {
+			posts: []
+		}
+	}
+	
+
+
+	componentDidMount() {
+		axios.get('http://localhost:8085/transactions', this.config)
+		.then(response => {
+			console.log(response)
+		})
+		.catch(error => {
+			console.log(error)
+		})
+	}
+
+	render() {
+		return ( <div> Lol </div> )
+	}
+}
+
+export default PostList
