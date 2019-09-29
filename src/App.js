269af89d8a9a45f04dfcdd1a0fcc6f13fb3bbeb9
pl0q1n <pl0q1n@gmail.com>
2019-09-29 23:16:37 +0400

    rewrite request form

--- src/App.js
+++ src/App.js
@@ -1,37 +1,11 @@
 import React from 'react';
 import './App.css';
-import PostList from './components/PostList'
-import Button from './components/Button'
 import RequestForm from './components/RequestForm'  
-import Transaction from './components/Transaction'
-
-var senderBox = new RequestForm("sender") 
-var receiverBox = new RequestForm("receiver")
-var valueBox = new RequestForm("value")
-var timeBox = new RequestForm("time")
-var categoryBox = new RequestForm("category")
-
-var onClick = (senderBox, receiverBox, valueBox, timeBox, categoryBox) => {
-  var trans = new Transaction(senderBox.getValue(), receiverBox.getValue(), 
-              valueBox.getValue(), timeBox.getValue(), categoryBox.getValue())
-
-  console.log(JSON.stringify(trans))
-}
 
 function App() { 
   return (
     <div>
-      <PostList />
-      {senderBox.render()}
-      {receiverBox.render()}
-      {valueBox.render()}
-      {timeBox.render()}
-      {categoryBox.render()}
-      <Button label={"Send transaction"} handleClick={onClick(senderBox, 
-                                                              receiverBox, 
-                                                              valueBox, 
-                                                              timeBox, 
-                                                              categoryBox)} />
+      <RequestForm /> 
     </div>
   );
 }
--- src/components/RequestForm.js
+++ src/components/RequestForm.js
@@ -1,29 +1,86 @@
 import React, { Component } from 'react'
+import Button from './Button'
+import Transaction from './Transaction'
 
 class RequestForm extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {value: ''};
-    this.boxName = props.boxName
-    this.handleChange = this.handleChange.bind(this);
+    this.state = {
+    };
+
+    this.handleInputChange = this.handleInputChange.bind(this);
   }
 
-  getValue() {
-    return this.state.value
+  handleInputChange(event) {
+    const target = event.target;
+    const value = target.value;
+    const name = target.name;
+
+    this.setState({
+      [name]: value
+    });
   }
 
-  handleChange(event) {
-    this.setState({value: event.target.value});
+  sendTransaction() {
+    let trans = Transaction(this.state.sender, 
+                            this.state.receiver, 
+                            this.state.value, 
+                            this.state.time, 
+                            this.state.category)
+    console.log(trans.getJson())
   }
 
   render() {
     return (
+      <form>
         <label>
-          <p>
-          {this.boxName}: &nbsp;
-          <input type="text" value={this.state.value} onChange={this.handleChange} />
-          </p>
+          Sender: &nbsp;
+          <input
+            name="Sender"
+            type="text"
+            value={this.state.sender}
+            onChange={this.handleInputChange} />
+            <br />
         </label>
+        <label>
+          Receiver: &nbsp;
+          <input
+            name="Receiver"
+            type="text"
+            value={this.state.receiver}
+            onChange={this.handleInputChange} />
+            <br />
+        </label>
+        <label>
+          Value: &nbsp;
+          <input
+            name="Value"
+            type="number"
+            value={this.state.value}
+            onChange={this.handleInputChange} />
+            <br />
+        </label>
+        <label>
+          Timestamp: &nbsp;
+          <input
+            name="Timestamp"
+            type="number"
+            value={this.state.timestamp}
+            onChange={this.handleInputChange} />
+            <br />
+        </label>
+        <label>
+          Category: &nbsp;
+          <input
+            name="Category"
+            type="text"
+            value={this.state.category}
+            onChange={this.handleInputChange} />
+            <br />
+        </label>
+        
+      <Button label={"Send transaction"} handleClick={this.sendTransaction}/>
+      </form>
     );
   }
 }
